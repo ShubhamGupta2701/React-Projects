2b2c151cabd517ff60b57b493988a5df313811f3
ShubhamGupta <92706349+ShubhamGupta2701@users.noreply.github.com>
2024-03-09 19:19:17 +0530

    Add files via upload

---
+++ todo-list/backend/db.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+//My_mongoose_url is mongoose url to connect the db with my backend
+
+// mongodb+srv://lucifer:lucifermrngstar@cluster0.5yvhitx.mongodb.net/
+mongoose.connect("mongodb+srv://lucifer:lucifermrngstar@cluster0.5yvhitx.mongodb.net/")
+
+const todoSchema = new mongoose.Schema({
+    title : String,
+    description : String,
+    isCompleted : Boolean
+    // isCompleted : {
+    //     type:Boolean,
+    //     // default:false
+    // }
+});
+
+const todo = mongoose.model("todo", todoSchema)
+module.exports = {todo:todo};
