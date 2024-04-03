4b4036fcf6bd58ae754605aeffa54a931aca2d27
ShubhamGupta <shubham272101@gmail.com>
2024-04-03 14:10:36 +0530

    Update db.js

--- todo-list/backend/db.js
+++ todo-list/backend/db.js
@@ -2,8 +2,8 @@ const mongoose = require("mongoose");
 
 //My_mongoose_url is mongoose url to connect the db with my backend
 
-// mongodb+srv://lucifer:lucifermrngstar@cluster0.5yvhitx.mongodb.net/
-mongoose.connect("mongodb+srv://lucifer:lucifermrngstar@cluster0.5yvhitx.mongodb.net/")
+// mongodb+srv://lucifer:<password>@cluster0.5yvhitx.mongodb.net/
+mongoose.connect("mongodb+srv://account:<password>@cluster0.5yvhitx.mongodb.net/")
 
 const todoSchema = new mongoose.Schema({
     title : String,
