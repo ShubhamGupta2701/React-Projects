3bdc7b7930d15349e985ed7e31b7d50a82a5e503
ShubhamGupta <shubham272101@gmail.com>
2024-04-08 02:27:12 +0530

    Add files via upload

--- routing-context-API/src/main.jsx
+++ routing-context-API/src/main.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import PropDrilling from './PropDrilling.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-    <App />
+    <PropDrilling />
 )
